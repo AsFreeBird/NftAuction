69daf10a1a39de6eac09a8f1b349aee443684b13
wupeng <603928546@qq.com>
2025-08-25 23:44:26 +0800

    出价

--- contracts/NftAuction.sol
+++ contracts/NftAuction.sol
@@ -3,6 +3,9 @@ pragma solidity ^0.8.28;
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
 
 contract NftAuction is Initializable,UUPSUpgradeable{
 
@@ -63,6 +66,81 @@ contract NftAuction is Initializable,UUPSUpgradeable{
         auctionId++;
     }
 
+    /**
+     * @notice ChainLink 价格源映射
+     * @dev address(0) 代表 ETH 价格源
+     */
+    mapping(address =>AggregatorV3Interface) public priceFeeds;
+
+    /**
+     *  给代币地址设置ChainLink价格源
+     * @param payToken 代币地址，address(0) 一般代表 ETH。
+     * @param priceFeed ChainLink 价格预言机地址
+     */
+    function setPricesFeed(address payToken,address priceFeed) external{
+        priceFeeds[payToken] = AggregatorV3Interface(priceFeed);
+    }
+    /**
+     * 
+     * @param payToken 代币地址，address(0) 一般代表 ETH。
+     * @return 返回代币的最新价格，8位小数
+     */
+    function getChainLinkFeedLatestPrice(address payToken) public view returns(uint256){
+        AggregatorV3Interface priceFeed = priceFeeds[payToken];
+        (,int256 price,,,) = priceFeed.latestRoundData();
+        return price;
+    }
+    /**
+     * 
+     * @param _auctionId 拍卖ID
+     * @param _bidAmount  出价金额
+     * @param _payToken  支付代币地址，address(0) 一般代表 ETH。
+     * @dev 竞拍
+     */
+    function placeBid(
+        uint256 _auctionId,
+        uint256 _bidAmount,
+        address _payToken
+        ) external payable{
+        //当前拍卖
+        Auction storage auction = auctions[_auctionId]; 
+        // 拍卖必须存在
+        require(auction.seller != address(0), "auction not exist");
+        // 拍卖未结束
+        require(!auction.ended && block.timestamp < auction.startTime +auction.duration,"auction is ended");
+        
+        uint256 payValue;
+        // 计算出价金额  根据_payToken算出美元价值
+        if(_payToken == address(0)){
+            _bidAmount = msg.value;
+            payValue = _bidAmount * uint(getChainLinkFeedLatestPrice(address(0)));
+        }else{
+            payValue = _bidAmount * uint(getChainLinkFeedLatestPrice(_payToken));
+        }
+        uint256 startPrice = auction.startPrice * uint(getChainLinkFeedLatestPrice(auction.payToken));
+        uint256 highestBid = auction.hinghestBid * uint(getChainLinkFeedLatestPrice(auction.payToken));
+        require(payValue >= startPrice && payValue >highestBid,"bid amount is too low");
+        
+        // 支付出价金额 转移 ERC20 到合约  
+        if(_payToken !=address(0)){
+            IERC20(_payToken).transferFrom(msg.sender,address(this),_bidAmount);
+        }        
+
+        // 如果有最高出价者，返还之前的出价
+        if(auction.highestBid > 0){
+            //出价是ETH 退还ETH
+            if(auction.payToken == address(0)){
+                payable(auction.hightestBidder).transfer(auction.highestBid);
+            }else{ // 出价是ERC20 退还ERC20
+                IERC20(auction.payToken).transfer(auction.hightestBidder,auction.highestBid);
+            }
+        }
+        //更新合约
+        auction.hinghestBid = _bidAmount;
+        auction.hightestBidder = msg.sender;
+        auction.payToken = _payToken;
+
+    }
 
 
 }
